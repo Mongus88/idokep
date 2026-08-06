257ee6630c4aaa29b93cde7c24ac8ceb8e7a5e07
Mongus88 <ivatam88@gmail.com>
2026-08-06 17:55:37 +0200

    trace view2

--- features/environment.py
+++ features/environment.py
@@ -1,5 +1,7 @@
 from playwright.sync_api import sync_playwright
 
+import re
+
 from pages.base_page import BasePage
 from pages.budapest_page import BudapestPage
 from pages.hun_heat_page import HunHeatPage
@@ -23,11 +25,9 @@ def before_all(context):
 
 
 def before_scenario(context, scenario):
-    context.page = context.browser_context.new_page()
+    context.browser_context.tracing.start(screenshots=True, snapshots=True, sources=True)
 
-    context.tracing_context = context.browser.new_context()
-    context.tracing_context.tracing.start(screenshots=True, snapshots=True, sources=True)
-    context.page = context.tracing_context.new_page()
+    context.page = context.browser_context.new_page()
 
     context.base_page = BasePage(context.page)
     context.precipitation_page = PrecipitationPage(context.page)
@@ -36,13 +36,15 @@ def before_scenario(context, scenario):
 
 
 def after_scenario(context, scenario):
+
+    clean_name = re.sub(r'[^\w\-_.]', '_', scenario.name)
+
     if scenario.status == "failed":
-        context.tracing_context.tracing.stop(path=f"trace_{scenario.name.replace(' ', '_')}.zip")
+        context.browser_context.tracing.stop(path=f"trace_{clean_name}.zip")
     else:
-        context.tracing_context.tracing.stop()
+        context.browser_context.tracing.stop()
 
     context.page.close()
-    context.tracing_context.close()
 
 
 def after_all(context):
