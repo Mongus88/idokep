cda371cdc607ea02331e271b5317a049bb374524
Mongus88 <ivatam88@gmail.com>
2026-08-07 11:29:41 +0200

    cookie_handler5

--- features/environment.py
+++ features/environment.py
@@ -11,28 +11,23 @@ from pages.precipitation_page import PrecipitationPage
 def before_all(context):
     context.playwright = sync_playwright().start()
     context.browser = context.playwright.chromium.launch(headless=True, slow_mo=500)
-
     context.browser_context = context.browser.new_context(viewport={"width": 1920, "height": 1080})
 
     context.browser_context.tracing.start(screenshots=True, snapshots=True, sources=True)
 
-    temp_page = context.browser_context.new_page()
-    temp_page.goto(f"{BasePage.base_url()}/idojaras/Budapest", wait_until="domcontentloaded")
-    try:
-        temp_page.locator("#accept-btn").click(timeout=5000)
-    except Exception:
-        pass
-
-    try:
-        temp_page.get_by_role("button", name="Confirm").click(timeout=5000)
-    except Exception:
-        pass
-
-        temp_page.close()
-
 
 def before_scenario(context, scenario):
     context.page = context.browser_context.new_page()
+
+    context.page.add_locator_handler(
+        context.page.locator("#accept-btn"),
+        lambda overlay: overlay.click()
+    )
+
+    context.page.add_locator_handler(
+        context.page.get_by_role("button", name="Confirm"),
+        lambda overlay: overlay.click()
+    )
 
     context.base_page = BasePage(context.page)
     context.precipitation_page = PrecipitationPage(context.page)
@@ -46,8 +41,13 @@ def after_scenario(context, scenario):
 
 def after_all(context):
     os.makedirs("traces", exist_ok=True)
-    context.browser_context.tracing.stop(path="traces/trace.zip")
 
-    context.browser_context.close()
-    context.browser.close()
-    context.playwright.stop()
+    if hasattr(context, "browser_context"):
+        context.browser_context.tracing.stop(path="traces/trace.zip")
+        context.browser_context.close()
+
+    if hasattr(context, "browser"):
+        context.browser.close()
+
+    if hasattr(context, "playwright"):
+        context.playwright.stop()
